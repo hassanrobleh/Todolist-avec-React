d51d5f993b8acee4eed712a57ffb9e178caca8aa
Hassan <hassan.alirobleh@gmail.com>
2021-04-17 20:37:51 +0200

    Présentation TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container p-5">
+        <h4>Ajouter une todo</h4>
+        <hr className="my-4" />
+        <div className="d-flex mb-4">
+          <input type="text" className="form-control mr-5"/>
+          <button className="btn btn-success">Ajouter</button>
+        </div>
+        <hr className="my-4" />
+        <div className="card">
+          <div className="card-header d-flex flex-row align-items-center">
+            <span className="flex-fill">Todo list</span>
+            <button className="btn btn-primary mr-2">Tout</button>
+            <button className="btn btn-primary mr-2">Fini</button>
+            <button className="btn btn-primary">En cours</button>
+          </div>
+          <div className="card-body">
+            <ul className="list-group">
+              <li className="list-group-item d-flex flex-row justify-content-between">
+                <span> x </span>
+                <span>
+                  <input className="mx-3" type="checkbox" />
+                  <button className="btn btn-sm btn-danger">delete</button>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  render() {
+    return (
+      <div className="container p-5">
+        <h4>Ajouter une todo</h4>
+        <hr className="my-4" />
+        <div className="d-flex mb-4">
+          <input type="text" className="form-control mr-5"/>
+          <button className="btn btn-success">Ajouter</button>
+        </div>
+        <hr className="my-4" />
+        <div className="card">
+          <div className="card-header d-flex flex-row align-items-center">
+            <span className="flex-fill">Todo list</span>
+            <button className="btn btn-primary mr-2">Tout</button>
+            <button className="btn btn-primary mr-2">Fini</button>
+            <button className="btn btn-primary">En cours</button>
+          </div>
+          <div className="card-body">
+            <ul className="list-group">
+              <li className="list-group-item d-flex flex-row justify-content-between">
+                <span> x </span>
+                <span>
+                  <input className="mx-3" type="checkbox" />
+                  <button className="btn btn-sm btn-danger">delete</button>
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
