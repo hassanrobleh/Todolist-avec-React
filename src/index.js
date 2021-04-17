62937c5d8cc00f05d3e9a2a1b3017254535a85fb
Hassan <hassan.alirobleh@gmail.com>
2021-04-18 01:21:33 +0200

    presentation de store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
+import { todoReducers } from './store/reducers';
+
+const store = createStore(todoReducers);
 
 ReactDOM.render(
   <React.StrictMode>
