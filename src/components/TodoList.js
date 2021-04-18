17c761c53404f58d0968eb0c29d01a06c0b07cd3
Hassan <hassan.alirobleh@gmail.com>
2021-04-18 02:35:48 +0200

    Mise en place de connect() et mapStateToProps

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,35 @@
 import React from "react";
 import TodoItem from "./TodoItem";
+import { connect } from "react-redux";
+import { visibilityFilter } from "../store/actions";
 
-const TodoList = (props) => {
+const TodoList = ({todos}) => {
   return (
     <ul className="list-group">
-        <TodoItem/>
+      {todos.state &&
+        todos.todos.map((t) => <TodoItem keys={t.name} todos={t} />)}
     </ul>
-    
   );
 };
 
-export default TodoList;
+export default connect((state) => {
+  const filter = state.filter;
+  let todos;
+  switch (filter) {
+    case visibilityFilter.SHOW_DONE: {
+      todos = state.todos.filter((t) => t.done);
+      break;
+    }
+    case visibilityFilter.SHOW_ACTIVE: {
+      todos = state.todos.filter((t) => !t.done);
+      break;
+    }
+    default: {
+      todos = state.todos;
+      break;
+    }
+  }
+  return {
+    todos,
+  };
+})(TodoList);
