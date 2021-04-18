53f74b1ab229423a427c6068b74dce364f3444ed
Hassan <hassan.alirobleh@gmail.com>
2021-04-19 01:24:46 +0200

    presentation applyMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,48 @@
-import * as reducers from './reducers';
+import * as reducers from "./reducers";
 import { combineReducers } from "redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 
 const todoReducers = combineReducers(reducers);
-const store = createStore(todoReducers);
+
+const middleware1 = (dispatch, getState) => (next) => (action) => {
+  console.log("action :", action);
+  return next(action);
+};
+
+const middleware2 = (dispatch, getState) => (next) => (action) => {
+  console.log("middleware2");
+  return next(action);
+};
+let store = createStore(
+  todoReducers,
+  applyMiddleware(middleware1, middleware2)
+);
+
+/* const middleware = (store) => (next) => (action) => {
+  // Do something
+  next(action);
+}; */
+
+/* const next = store.dispatch;
+store.dispatch = (action) => {
+  console.log("action :", action);
+  return next(action);
+}; */
+
+/* middleware1(store);
+middleware2(store); */
+
+/* const myMiddleware = (store, middlewares) => {
+  const mids = middlewares.slice();
+  mids.reverse();
+  let dispatch = store.dispatch;
+  mids.forEach((m) => (dispatch = m(store)(dispatch)));
+  return {
+    ...store,
+    dispatch,
+  };
+}; */
+
+//store = myMiddleware(store, [middleware1, middleware2]);
 
 export default store;
