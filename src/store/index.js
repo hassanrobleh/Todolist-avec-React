c8fbb6b9e27f754e052e713ec6ff5c139564e412
Hassan <hassan.alirobleh@gmail.com>
2021-04-19 02:12:44 +0200

    Redux-logger et devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,23 @@
 import * as reducers from "./reducers";
 import { combineReducers } from "redux";
 import { createStore, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 const todoReducers = combineReducers(reducers);
 
-const middleware1 = (dispatch, getState) => (next) => (action) => {
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
+
+const store = createStore(
+  todoReducers,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
+
+/* const middleware1 = (dispatch, getState) => (next) => (action) => {
   console.log("action :", action);
   return next(action);
 };
@@ -16,7 +29,7 @@ const middleware2 = (dispatch, getState) => (next) => (action) => {
 let store = createStore(
   todoReducers,
   applyMiddleware(middleware1, middleware2)
-);
+); */
 
 /* const middleware = (store) => (next) => (action) => {
   // Do something
